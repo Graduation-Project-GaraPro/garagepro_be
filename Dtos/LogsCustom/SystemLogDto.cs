fa88086ef17b5e788a867d8587645a14d0465ea3
letienks2410 <anhemgameplay@gmail.com>
2025-10-21 16:52:52 +0700

    fixIgnore log

---
+++ Dtos/LogsCustom/SystemLogDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dtos.Logs
+{
+    public class SystemLogDto
+    {
+        public long Id { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+        public string Level { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? Details{ get; set; } = string.Empty;
+        public string? IpAddress { get; set; }
+        public string? UserAgent { get; set; }
+        public string? RequestId { get; set; }
+    }
+}
