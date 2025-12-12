8a0b71677249198e8c44ab9da7e3d0a46c675ff9
Hoangminhfcb <78634684+Hoangminhfcb@users.noreply.github.com>
2025-12-12 19:47:20 +0700

    fix seed db

--- DataAccessLayer/Migrations/20251212124548_InitialCreate.Designer.cs
+++ DataAccessLayer/Migrations/20251212124548_InitialCreate.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(MyAppDbContext))]
-    [Migration("20251212103831_addnew")]
-    partial class addnew
+    [Migration("20251212124548_InitialCreate")]
+    partial class InitialCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
